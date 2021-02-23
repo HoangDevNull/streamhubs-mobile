ee86a99f1f8e01a86b5fe65532be5533597b6aa7
HoangDevNull <pqhoang.17it2@sict.udn.vn>
2021-02-23 21:35:55 +0700

    init

--- android/app/src/main/java/com/myapp/MainActivity.java
+++ android/app/src/main/java/com/myapp/MainActivity.java
@@ -4,12 +4,24 @@ import com.facebook.react.ReactActivity;
 // react-native-splash-screen >= 0.3.1
 import org.devio.rn.splashscreen.SplashScreen; 
 
+import android.content.Intent;
+import android.content.res.Configuration;
+
 public class MainActivity extends ReactActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     SplashScreen.show(this, R.style.SplashScreenTheme);
     super.onCreate(savedInstanceState);
   }
+
+  @Override
+  public void onConfigurationChanged(Configuration newConfig) {
+    super.onConfigurationChanged(newConfig);
+    Intent intent = new Intent("onConfigurationChanged");
+    intent.putExtra("newConfig", newConfig);
+    this.sendBroadcast(intent);
+  }
+
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
